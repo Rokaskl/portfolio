3855a7329c4be39f465b83cbb923aa6cab9f79ac
rokaskl <rokasklkl@gmail.com>
2020-06-25 17:52:54 +0300

    submit method

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,29 @@
 import React from "react";
 
 export default function Contact() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...formValues,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch((error) => alert(error));
+  };
   return (
     <section id="contact">
       <h2>Contact Me</h2>
-      <form name="contact" method="POST" data-netlify="true">
+      <form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        onSubmit={handleSubmit}
+      >
         <label>Name</label>
         <input type="text" name="name" />
         <label>Email</label>
